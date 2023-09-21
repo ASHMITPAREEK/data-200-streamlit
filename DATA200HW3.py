c2cf4ba84d4b193f683a99e7e1354e6ba7565fdb
ASHMITPAREEK <143358318+ASHMITPAREEK@users.noreply.github.com>
2023-09-21 14:56:21 -0700

    Update DATA200HW3.py

--- DATA200HW3.py
+++ DATA200HW3.py
@@ -1,12 +1,19 @@
+import streamlit as st
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
-file_path = 'https://github.com/ASHMITPAREEK/data-200-streamlit/blob/main/toy_dataset.csv'
+
+path = os.path.dirname(__file__)
+file_path =  path + '/toy_dataset.csv'
 data = pd.read_csv(file_path)
 data.head()
+
 gen_count = data['Gender'].value_counts()
 
-plt.bar(gen_count.index, gen_count.values, color='skyblue')
+plt.bar(gen_count.index, gen_count.values, color='Red')
 plt.xlabel('Gender')
 plt.ylabel('Count')
-plt.title('Distribution of Gender in Toy Dataset')
+plt.title('Distribution of Gender in Dataset')
 plt.show()
+
+st.pyplot(plt)
